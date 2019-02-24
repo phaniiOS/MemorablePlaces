414f0e5cf2777f332d8c86b501cf1a6e34869774
Phanindranath Thota <phanindranath.wg113@gmail.com>
2019-02-24 08:58:47 -0600

    Changes

---
+++ MemorablePlaces/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  MemorablePlaces
+//
+//  Created by IMCS2 on 2/24/19.
+//  Copyright © 2019 IMCS2. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+var latitudes: [CLLocationDegrees] = []
+var longitudes: [CLLocationDegrees] = []
+var titles: [String] = []
+var subTitles: [String] = []
+var noData: Bool = false
